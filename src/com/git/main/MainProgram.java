cda6adf01f8dc6386530d1e7515914a8e8ad362d
Training <training@bootcamp-wheezy>
2013-06-05 11:36:09 +0800

    Initial Commit

---
+++ src/com/git/main/MainProgram.java
@@ -0,0 +1,13 @@
+package com.git.main;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class MainProgram {
+	
+	@Test
+	public void helloWorld(){
+		Assert.assertTrue(true);
+	}
+}
