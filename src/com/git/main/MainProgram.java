8706718e3dd70c59045650e5990c74413ba43fb8
BK <biksantiago@gmail.com>
2013-06-05 14:03:36 +0800

    pang expeirment branch

--- src/com/git/main/MainProgram.java
+++ src/com/git/main/MainProgram.java
@@ -11,5 +11,10 @@ public class MainProgram {
 		Assert.assertEquals(1, 2);
 		Assert.assertEquals(4, 2);
 		Assert.assertTrue(false);
+		
+		/* PANG EXPERIMENT BRANCH*/
+		int b = 6;
+		int sum = b += 1;
+		System.out.println(sum);
 	}
 }
